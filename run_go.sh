b7444c61662a2364918fa98a760d6b01480783ee
unipark00 <unipark00@naver.com>
2019-04-25 12:12:14 +0900

    modify run_go.sh

--- run_go.sh
+++ run_go.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-export GOPATH=/home/ejungpa/git/go-api-server
-#go env | grep GOPATH
+export GOPATH=/home/ejungpa/git/go-server-server
+go env | grep GOPATH
+go get -v -u github.com/gorilla/mux
 go run main.go

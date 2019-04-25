a138169564925e1ce4fea4e81959f26b182965d9
unipark00 <unipark00@naver.com>
2019-04-25 12:22:48 +0900

    modify script

--- run_go_init.sh
+++ run_go_init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export GOPATH=/home/ejungpa/git/go-server-server
+export GOPATH=/home/ejungpa/git/go-api-server
 #go env | grep GOPATH
 go get -v -u github.com/gorilla/mux
 go run main.go
